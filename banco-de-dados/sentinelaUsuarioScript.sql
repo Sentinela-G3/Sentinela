139d79f8c6d873fba4df6196d372e1bbdf894f78
lucaspaessptech <lucas.paes@sptech.school>
2025-02-28 13:26:14 -0300

    Criação do usuário de insert cadastro

--- banco-de-dados/sentinelaUsuarioScript.sql
+++ banco-de-dados/sentinelaUsuarioScript.sql
@@ -1,9 +1,20 @@
-create user 'sentinelaInsert'@'%' identified by 'Sentinela@123';
+create user 'sentinelaInsertDados'@'%' identified by 'Sentinela@123';
 
-grant insert on sentinela.* to 'sentinelaInsert'@'%';
+grant insert on sentinela.processador to 'sentinelaInsertDados'@'%';
+grant insert on sentinela.memoria to 'sentinelaInsertDados'@'%';
+grant insert on sentinela.bateria to 'sentinelaInsertDados'@'%';
+grant insert on sentinela.redeChip to 'sentinelaInsertDados'@'%';
+grant insert on sentinela.armazenamento to 'sentinelaInsertDados'@'%';
 
 create user 'sentinelaSelect'@'%' identified by 'Sentinela@123';
 
 grant select on sentinela.* to 'sentinelaSelect'@'%';
+
+create user 'sentinelaInsertCadastro'@'%' identified by 'Sentinela@123';
+
+grant insert on sentinela.usuario to 'sentinelaInsertCadastro'@'%';
+grant insert on sentinela.endereco to 'sentinelaInsertCadastro'@'%';
+grant insert on sentinela.empresa to 'sentinelaInsertCadastro'@'%';
+grant insert on sentinela.usuarioEndereco to 'sentinelaInsertCadastro'@'%';
 
 flush privileges;
