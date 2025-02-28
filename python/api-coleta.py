dc68a1d4db3abb9fdd59a31198664de652a9ccc0
gabrielpacificoo <gabriel.pacifico@sptech.school>
2025-02-28 12:49:55 -0300

    Inserção das variaveis na API-Coleta e compatibilidade com Windows e Linux

--- python/api-coleta.py
+++ python/api-coleta.py
@@ -3,16 +3,22 @@ import time
 import mysql.connector
 import datetime
 
-mydb = mysql.connector.connect(
-  host="10.18.32.10",
-  user="sentinelaInsert",
-  password="Sentinela@123",
-  database="sentinela"
-)
+from mysql.connector import (connection)
+# mydb = connection.MySQLConnection(host='10.18.32.48', user='sentinelaInsert', password='Sentinela@123', database='sentinela')
+mydb = connection.MySQLConnection(host='localhost', user='root', password='Gg1502@#', database='sentinela')
 
 mycursor = mydb.cursor()
-
 tempoSegundos = int(input("Insira quantos segundos deseja monitorar: "))
+
+isLinux = psutil.LINUX
+
+#Configuração da máquina
+fkMaquina = 1
+fkComponenteCPU = 1
+fkComponenteRAM = 2
+fkComponenteREDE = 4
+fkComponenteBATERIA = 3
+fkComponenteDISCO = 5
 
 while tempoSegundos > 0:
     tempoSegundos-= 1
@@ -24,14 +30,8 @@ while tempoSegundos > 0:
     porcentagemUsoProcessador = psutil.cpu_percent()
     frequenciaProcessador = psutil.cpu_freq().current
 
-    print("Processador:\n")
-    print("Porcentagem de uso do processador: ", porcentagemUsoProcessador)
-    print("Tempo que o processador passou realizando operações (em porcentagem): ", tempoAtivo)
-    print("Tempo que o processador permaneceu inativo (em porcentagem): ", tempoInativo)
-    print("Frequência do processador: ", frequenciaProcessador, "\n")
-    
-    sql = "INSERT INTO processador VALUES (default, %s, %s, %s, %s, %s, 1, 1)"
-    val = (porcentagemUsoProcessador, tempoAtivo, tempoInativo, frequenciaProcessador, now)
+    sql = "INSERT INTO processador VALUES (default, %s, %s, %s, %s, %s, 1, %s)"
+    val = (porcentagemUsoProcessador, tempoAtivo, tempoInativo, frequenciaProcessador, now, fkComponenteCPU)
     mycursor.execute(sql, val)
 
     mydb.commit()
@@ -41,50 +41,58 @@ while tempoSegundos > 0:
     memoriaNaousada = psutil.virtual_memory().free
     memoriaTotal = psutil.virtual_memory().total
 
+    sql = "INSERT INTO memoria VALUES (default, %s, %s, %s, %s, %s, 1, %s)"
+    val = (memoriaNaousada, memoriaDisponivel, memoriaTotal, memoriaUtilizadaporcentagem, now, fkComponenteRAM)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+    bateriaPorcentagematual = psutil.sensors_battery().percent
+    # bateriaTemporestante = psutil.sensors_battery().secsleft
+
+    sql = "INSERT INTO bateria VALUES (default, %s, 0, %s, 1, %s)"
+    val = (bateriaPorcentagematual, now, fkComponenteBATERIA)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+    redeBytesenviados = psutil.net_io_counters().bytes_sent
+
+    sql = "INSERT INTO redeChip VALUES (default, %s, %s, 1, %s)"
+    val = (redeBytesenviados, now, fkComponenteREDE)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+    if isLinux:
+        armazenamentoDisponivel = psutil.disk_usage('/').free
+        armazenamentoTotal = psutil.disk_usage('/').total        
+    else:
+        armazenamentoDisponivel = psutil.disk_usage('C:').free
+        armazenamentoTotal = psutil.disk_usage('C:').total
+
+    sql = "INSERT INTO armazenamento VALUES (default, %s, %s, %s, 1, %s)"
+    val = (armazenamentoTotal, armazenamentoDisponivel, now, fkComponenteDISCO)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    
+    print("Processador:\n")
+    print("Porcentagem de uso do processador: ", porcentagemUsoProcessador)
+    print("Tempo que o processador passou realizando operações (em porcentagem): ", tempoAtivo)
+    print("Tempo que o processador permaneceu inativo (em porcentagem): ", tempoInativo)
+    print("Frequência do processador: ", frequenciaProcessador, "\n")
+
     print("Memória Ram\n")
     print("Quantidade de memória disponível: ", memoriaDisponivel)
     print("Quantidade de memória utilizada em porcentagem: ", memoriaUtilizadaporcentagem)
     print("Quantidade de memória que permaneceu não utilizada: ", memoriaNaousada)
     print("Quantidade de memoria total: ", memoriaTotal, "\n")
 
-    sql = "INSERT INTO memoria VALUES (default, %s, %s, %s, %s, %s, 1, 1)"
-    val = (memoriaNaousada, memoriaDisponivel, memoriaTotal, memoriaUtilizadaporcentagem, now)
-    mycursor.execute(sql, val)
-    mydb.commit()
-
-    bateriaPorcentagematual = psutil.sensors_battery().percent
-    bateriaTemporestante = psutil.sensors_battery().secsleft
-
     print("Bateria\n")
     print("Porcentagem da carga atual da bateria: ", bateriaPorcentagematual)
-    print("Tempo Restante em segundos da bateria: ", bateriaTemporestante, "\n")
-    sql = "INSERT INTO bateria VALUES (default, %s, %s, %s, 1, 1)"
-    val = (bateriaPorcentagematual, bateriaTemporestante, now)
-    mycursor.execute(sql, val)
-    mydb.commit()
-
-    redeBytesenviados = psutil.net_io_counters().bytes_sent
 
     print("Rede\n")
-
     print("Quantidade de bytes enviados: ", redeBytesenviados, "\n")
-    
-    sql = "INSERT INTO redeChip VALUES (default, %s, %s, 1, 1)"
-    val = (redeBytesenviados, now)
-    mycursor.execute(sql, val)
-    mydb.commit()
-
-    armazenamentoDisponivel = psutil.disk_usage('C:').free
-    armazenamentoTotal = psutil.disk_usage('C:').total
 
     print("Armazenamento\n")
-
     print("Quantidade de armazenamento disponivel: ", armazenamentoDisponivel)
     print("Quantidade total de armazenamento: ", armazenamentoTotal, "\n")
 
-    sql = "INSERT INTO armazenamento VALUES (default, %s, %s, %s, 1, 1)"
-    val = (armazenamentoTotal, armazenamentoDisponivel, now)
-    mycursor.execute(sql, val)
-    mydb.commit()
-
-    time.sleep(1)
+    time.sleep(15)
