d04e7228664ad31b01bed8f3fdf274c5a21c24d5
lucaspaessptech <lucas.paes@sptech.school>
2025-03-09 03:21:28 -0300

    idEmpresa sendo armazenado no sessionStorage

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -14,10 +14,13 @@ function autenticar(req, res) {
             .then(
                 function (resultadoAutenticar) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
-                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`);
 
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
+                        res.json({
+                            fkEmpresa: resultadoAutenticar[0].idEmpresa
+                        })
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
