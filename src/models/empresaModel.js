c75b35e3335fd69cafb09074989a4aab5dc4c476
lucaspaessptech <lucas.paes@sptech.school>
2025-03-12 21:38:29 -0300

    Rotas funcionando, vou realizar ajustes para o armazenamento do fkEndereço no sessionStorage

--- src/models/empresaModel.js
+++ src/models/empresaModel.js
@@ -1,14 +1,14 @@
 var database = require("../database/config");
 
 function cadastrar(nome, cnpj, categoria) {
-  var instrucaoSql = `INSERT INTO empresa (nomeFantasia, cnpj, categoria, dataInicio) VALUES ('${nome}', '${cnpj}', ${categoria}, now());`;
-
+  var instrucaoSql = `INSERT INTO empresa (nomeFantasia, cnpj, categoria, dataInicio) VALUES ('${nome}', '${cnpj}', '${categoria}', now());`;
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
 }
 
 function cadastrarEndereco(logradouro, cep, estado, complemento){
   var instrucaoSql = `INSERT INTO endereco (logradouro, cep, estado, complemento) VALUES ('${logradouro}', '${cep}', '${estado}', '${complemento}');`;
-
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
 }
 
