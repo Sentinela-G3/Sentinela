b6db2c79a09335fc38da240d76a20710e0580586
lucaspaessptech <lucas.paes@sptech.school>
2025-03-16 16:39:23 -0300

    Senhas criptografadas

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function autenticar(): ", email, senha)
     var instrucaoSql = `
-        SELECT idUsuario, nome, email, fkCargo, idEmpresa, fkDono FROM usuario join usuarioEndereco on idUsuario = fkUsuario join endereco on idEndereco = fkEndereco join empresa on idEmpresa = fkEmpresa WHERE email = '${email}' AND senha = '${senha}' limit 1;
+        SELECT idUsuario, nome, email, fkCargo, idEmpresa, fkDono FROM usuario join usuarioEndereco on idUsuario = fkUsuario join endereco on idEndereco = fkEndereco join empresa on idEmpresa = fkEmpresa WHERE email = '${email}' AND senha = SHA2('${senha}', 256) limit 1;
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -12,7 +12,7 @@ function autenticar(email, senha) {
 function cadastrar(nome, email, cpf, contato, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, cpf, contato, senha);
     var instrucaoSql = `
-        INSERT INTO usuario (nome, email, cpf, contato, senha, fkCargo) VALUES ('${nome}', '${email}', '${cpf}', '${contato}', '${senha}', 1);
+        INSERT INTO usuario (nome, email, cpf, contato, senha, fkCargo) VALUES ('${nome}', '${email}', '${cpf}', '${contato}', SHA2('${senha}', 256), 1);
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
 
