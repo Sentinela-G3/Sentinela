0a008c43684963b738b923383baff5a437d2048a
DYTateno <diogo.tateno@sptech.school>
2025-03-15 22:51:32 -0300

    Aplicação Java

--- javaFaker/src/main/java/Dados.java
+++ javaFaker/src/main/java/Dados.java
@@ -3,152 +3,123 @@ import com.github.javafaker.Faker;
 public class Dados {
     Faker faker = new Faker();
 
+    private String ID;
+    private String CPU_frequencia;
+    private String RAM_utilizada;
+    private String RAM_naoutilizada;
+    private String RAM_total;
+    private String RAM_disponivel;
+    private String tempoAtivo;
+    private String tempoInativo;
+    private String tempoCPU;
+    private String ArmazenamentoDisponivel;
+    private String ArmazenamentoIndisponivel;
+    private String ArmazenamentoTotal;
+    private String Bateria;
 
-
-    private Double ID;
-    private Double CPU_frequencia;
-    private Double RAM_utilizada;
-    private Double RAM_naoutilizada;
-    private Double RAM_total;
-    private Double RAM_disponivel;
-    private Double tempoAtivo;
-    private Double tempoInativo;
-    private Double tempoCPU;
-    private Double ArmazenamentoDisponivel;
-    private Double ArmazenamentoIndisponivel;
-    private Double ArmazenamentoTotal;
-    private Double Bateria;
-
-    private Integer CPU_Porcentos;
-    private Integer RAM_utilizadaPorcento;
-    private Integer RAM_naoutilizadaPorcento;
-    private Integer ArmazenamentoIndisponivelPorcento;
+    private String CPU_Porcentos;
+    private String RAM_utilizadaPorcento;
+    private String RAM_naoutilizadaPorcento;
+    private String ArmazenamentoIndisponivelPorcento;
 
     public Dados() {
-
-        this.ID = faker.number().randomDouble(0,0,100);
-        this.CPU_frequencia = faker.number().randomDouble(2, 0, (int) 5.0);
-        this.RAM_utilizada = faker.number().randomDouble(2, 0, 32);
-        this.RAM_naoutilizada = faker.number().randomDouble(2, 0, 32);
-        this.RAM_total = 32.0;
-        this.RAM_disponivel = this.RAM_total - this.RAM_utilizada;
-        this.tempoAtivo = faker.number().randomDouble(2, 1, 24);
-        this.tempoInativo = faker.number().randomDouble(2, 1, 24);
-        this.tempoCPU = faker.number().randomDouble(2, 0, 24);
-        this.ArmazenamentoIndisponivel = faker.number().randomDouble(2, 0, 1000);
-        this.ArmazenamentoTotal = 1000.0;
-        this.ArmazenamentoDisponivel = this.ArmazenamentoTotal - this.ArmazenamentoIndisponivel;
-        this.Bateria = faker.number().randomDouble(0, 0, 100);
-
-
-        CPU_Porcentos = (int)((this.CPU_frequencia *100) / 5.0) ;
-
-        RAM_utilizadaPorcento = (int) ((this.RAM_utilizada * 100) / this.RAM_total);
-
-        RAM_naoutilizadaPorcento = (int)((this.RAM_naoutilizada *100) / this.RAM_total);
-
-        ArmazenamentoIndisponivelPorcento = (int)((this.ArmazenamentoIndisponivel *100) / this.ArmazenamentoTotal);
-
-
-
+        gerarDados();
     }
 
-    public Integer repetir(Integer vezes){
+    private void gerarDados() {
+        this.ID = String.valueOf(faker.number().randomNumber());
+        this.CPU_frequencia = String.valueOf(faker.number().randomDouble(2, 0, 5));
+        this.RAM_utilizada = String.valueOf(faker.number().randomDouble(2, 0, 32));
+        this.RAM_naoutilizada = String.valueOf(faker.number().randomDouble(2, 0, 32));
+        this.RAM_total = "32";
+        this.RAM_disponivel = String.valueOf(Double.parseDouble(this.RAM_total) - Double.parseDouble(this.RAM_utilizada));
+        this.tempoAtivo = String.valueOf(faker.number().randomDouble(2, 1, 24));
+        this.tempoInativo = String.valueOf(faker.number().randomDouble(2, 1, 24));
+        this.tempoCPU = String.valueOf(faker.number().randomDouble(2, 0, 24));
+        this.ArmazenamentoIndisponivel = String.valueOf(faker.number().randomDouble(2, 0, 1000));
+        this.ArmazenamentoTotal = "1000";
+        this.ArmazenamentoDisponivel = String.valueOf(Double.parseDouble(this.ArmazenamentoTotal) - Double.parseDouble(this.ArmazenamentoIndisponivel));
+        this.Bateria = String.valueOf(faker.number().randomDouble(0, 0, 100));
+
+        // Calculando percentuais e armazenando como String
+        this.CPU_Porcentos = String.valueOf((int) ((Double.parseDouble(this.CPU_frequencia) * 100) / 5));
+        this.RAM_utilizadaPorcento = String.valueOf((int) ((Double.parseDouble(this.RAM_utilizada) * 100) / Double.parseDouble(this.RAM_total)));
+        this.RAM_naoutilizadaPorcento = String.valueOf((int) ((Double.parseDouble(this.RAM_naoutilizada) * 100) / Double.parseDouble(this.RAM_total)));
+        this.ArmazenamentoIndisponivelPorcento = String.valueOf((int) ((Double.parseDouble(this.ArmazenamentoIndisponivel) * 100) / Double.parseDouble(this.ArmazenamentoTotal)));
+    }
+
+    public void repetir(int vezes) {
         for (int i = 0; i < vezes; i++) {
-
-            this.ID = faker.number().randomDouble(0,1,0);
-            this.CPU_frequencia = faker.number().randomDouble(2, 0, (int) 5.0);
-            this.RAM_utilizada = faker.number().randomDouble(2, 0, 32);
-            this.RAM_naoutilizada = faker.number().randomDouble(2, 0, 32);
-            this.RAM_total = 32.0;
-            this.RAM_disponivel = this.RAM_total - this.RAM_utilizada;
-            this.tempoAtivo = faker.number().randomDouble(2, 1, 24);
-            this.tempoInativo = faker.number().randomDouble(2, 1, 24);
-            this.tempoCPU = faker.number().randomDouble(2, 0, 24);
-            this.ArmazenamentoIndisponivel = faker.number().randomDouble(2, 0, 1000);
-            this.ArmazenamentoTotal = 1000.0;
-            this.ArmazenamentoDisponivel = this.ArmazenamentoTotal - this.ArmazenamentoIndisponivel;
-            this.Bateria = faker.number().randomDouble(0, 0, 100);
-
-
-            CPU_Porcentos = (int)((this.CPU_frequencia *100) / 5.0) ;
-
-            RAM_utilizadaPorcento = (int) ((this.RAM_utilizada * 100) / this.RAM_total);
-
-            RAM_naoutilizadaPorcento = (int)((this.RAM_naoutilizada *100) / this.RAM_total);
-
-            ArmazenamentoIndisponivelPorcento = (int)((this.ArmazenamentoIndisponivel *100) / this.ArmazenamentoTotal);
-
+            gerarDados();
         }
-        return 0;
-    };
+    }
 
-    public Double getID() {
+    // Getters
+    public String getID() {
         return ID;
     }
 
-    public Double getCPU_frequencia() {
+    public String getCPU_frequencia() {
         return CPU_frequencia;
     }
 
-    public Double getRAM_utilizada() {
+    public String getRAM_utilizada() {
         return RAM_utilizada;
     }
 
-    public Double getRAM_naoutilizada() {
+    public String getRAM_naoutilizada() {
         return RAM_naoutilizada;
     }
 
-    public Double getRAM_total() {
+    public String getRAM_total() {
         return RAM_total;
     }
 
-    public Double getRAM_disponivel() {
+    public String getRAM_disponivel() {
         return RAM_disponivel;
     }
 
-    public Double getTempoAtivo() {
+    public String getTempoAtivo() {
         return tempoAtivo;
     }
 
-    public Double getTempoInativo() {
+    public String getTempoInativo() {
         return tempoInativo;
     }
 
-    public Double getTempoCPU() {
+    public String getTempoCPU() {
         return tempoCPU;
     }
 
-    public Double getArmazenamentoDisponivel() {
+    public String getArmazenamentoDisponivel() {
         return ArmazenamentoDisponivel;
     }
 
-    public Double getArmazenamentoIndisponivel() {
+    public String getArmazenamentoIndisponivel() {
         return ArmazenamentoIndisponivel;
     }
 
-    public Double getArmazenamentoTotal() {
+    public String getArmazenamentoTotal() {
         return ArmazenamentoTotal;
     }
 
-    public Double getBateria() {
+    public String getBateria() {
         return Bateria;
     }
 
-    public Integer getCPU_Porcentos() {
+    public String getCPU_Porcentos() {
         return CPU_Porcentos;
     }
 
-    public Integer getRAM_utilizadaPorcento() {
+    public String getRAM_utilizadaPorcento() {
         return RAM_utilizadaPorcento;
     }
 
-    public Integer getRAM_naoutilizadaPorcento() {
+    public String getRAM_naoutilizadaPorcento() {
         return RAM_naoutilizadaPorcento;
     }
 
-    public Integer getArmazenamentoIndisponivelPorcento() {
+    public String getArmazenamentoIndisponivelPorcento() {
         return ArmazenamentoIndisponivelPorcento;
     }
 }
-
--- javaFaker/src/main/java/Main.java
+++ javaFaker/src/main/java/Main.java
@@ -10,76 +10,67 @@ public class Main {
 
         Integer repeticoes = input.nextInt();
 
+        Dados dadosMaquina1 = new Dados();
 
-            Dados dadosMaquina1 = new Dados();
+        List<String> ID = new ArrayList<>();
+        List<String> CPU_frequencia = new ArrayList<>();
+        List<String> RAM_utilizada = new ArrayList<>();
+        List<String> RAM_naoutilizada = new ArrayList<>();
+        List<String> RAM_total = new ArrayList<>();
+        List<String> RAM_disponivel = new ArrayList<>();
+        List<String> tempoAtivo = new ArrayList<>();
+        List<String> tempoInativo = new ArrayList<>();
+        List<String> tempoCPU = new ArrayList<>();
+        List<String> ArmazenamentoDisponivel = new ArrayList<>();
+        List<String> ArmazenamentoIndisponivel = new ArrayList<>();
+        List<String> ArmazenamentoTotal = new ArrayList<>();
+        List<String> Bateria = new ArrayList<>();
+        List<String> CPU_Porcentos = new ArrayList<>();
+        List<String> RAM_utilizadaPorcento = new ArrayList<>();
+        List<String> RAM_naoutilizadaPorcento = new ArrayList<>();
+        List<String> ArmazenamentoIndisponivelPorcento = new ArrayList<>();
 
+        List<List<String>> Componentes = new ArrayList<>();
 
+        Componentes.add(ID);
+        Componentes.add(CPU_frequencia);
+        Componentes.add(RAM_utilizada);
+        Componentes.add(RAM_naoutilizada);
+        Componentes.add(RAM_total);
+        Componentes.add(RAM_disponivel);
+        Componentes.add(tempoAtivo);
+        Componentes.add(tempoInativo);
+        Componentes.add(tempoCPU);
+        Componentes.add(ArmazenamentoDisponivel);
+        Componentes.add(ArmazenamentoIndisponivel);
+        Componentes.add(ArmazenamentoTotal);
+        Componentes.add(Bateria);
+        Componentes.add(CPU_Porcentos);
+        Componentes.add(RAM_utilizadaPorcento);
+        Componentes.add(RAM_naoutilizadaPorcento);
+        Componentes.add(ArmazenamentoIndisponivelPorcento);
 
+        for (int i = 0; i < repeticoes; i++) {
+            dadosMaquina1.repetir(repeticoes);
+            ID.add(dadosMaquina1.getID());
+            CPU_frequencia.add(dadosMaquina1.getCPU_frequencia());
+            RAM_utilizada.add(dadosMaquina1.getRAM_utilizada());
+            RAM_naoutilizada.add(dadosMaquina1.getRAM_naoutilizada());
+            RAM_total.add(dadosMaquina1.getRAM_total());
+            RAM_disponivel.add(dadosMaquina1.getRAM_disponivel());
+            tempoAtivo.add(dadosMaquina1.getTempoAtivo());
+            tempoInativo.add(dadosMaquina1.getTempoInativo());
+            tempoCPU.add(dadosMaquina1.getTempoCPU());
+            ArmazenamentoDisponivel.add(dadosMaquina1.getArmazenamentoDisponivel());
+            ArmazenamentoIndisponivel.add(dadosMaquina1.getArmazenamentoIndisponivel());
+            ArmazenamentoTotal.add(dadosMaquina1.getArmazenamentoTotal());
+            Bateria.add(dadosMaquina1.getBateria());
+            CPU_Porcentos.add(dadosMaquina1.getCPU_Porcentos());
+            RAM_utilizadaPorcento.add(dadosMaquina1.getRAM_utilizadaPorcento());
+            RAM_naoutilizadaPorcento.add(dadosMaquina1.getRAM_naoutilizadaPorcento());
+            ArmazenamentoIndisponivelPorcento.add(dadosMaquina1.getArmazenamentoIndisponivelPorcento());
+        }
 
-
-            List<Double>ID = new ArrayList<>();
-            List<Double> CPU_frequencia = new ArrayList<>();
-            List<Double> RAM_utilizada = new ArrayList<>();
-            List<Double> RAM_naoutilizada = new ArrayList<>();
-            List<Double> RAM_total = new ArrayList<>();
-            List<Double> RAM_disponivel = new ArrayList<>();
-            List<Double> tempoAtivo = new ArrayList<>();
-            List<Double> tempoInativo = new ArrayList<>();
-            List<Double> tempoCPU = new ArrayList<>();
-            List<Double> ArmazenamentoDisponivel = new ArrayList<>();
-            List<Double> ArmazenamentoIndisponivel = new ArrayList<>();
-            List<Double> ArmazenamentoTotal = new ArrayList<>();
-            List<Double> Bateria = new ArrayList<>();
-            List<Double> CPU_Porcentos = new ArrayList<>();
-            List<Double> RAM_utilizadaPorcento = new ArrayList<>();
-            List<Double> RAM_naoutilizadaPorcento = new ArrayList<>();
-            List<Double> ArmazenamentoIndisponivelPorcento = new ArrayList<>();
-
-            List<List> Componentes = new ArrayList<>();
-
-            Componentes.add(ID);
-            Componentes.add(CPU_frequencia);
-            Componentes.add(RAM_utilizada);
-            Componentes.add(RAM_naoutilizada);
-            Componentes.add(RAM_total);
-            Componentes.add(RAM_disponivel);
-            Componentes.add(tempoAtivo);
-            Componentes.add(tempoInativo);
-            Componentes.add(tempoCPU);
-            Componentes.add(ArmazenamentoDisponivel);
-            Componentes.add(ArmazenamentoIndisponivel);
-            Componentes.add(ArmazenamentoTotal);
-            Componentes.add(Bateria);
-            Componentes.add(CPU_Porcentos);
-            Componentes.add(RAM_utilizadaPorcento);
-            Componentes.add(RAM_naoutilizadaPorcento);
-            Componentes.add(ArmazenamentoIndisponivelPorcento);
-
-
-
-            for (int i = 0; i < repeticoes; i++) {
-                dadosMaquina1.repetir(repeticoes);
-                ID.add(dadosMaquina1.getID());
-                CPU_frequencia.add(dadosMaquina1.getCPU_frequencia());
-                RAM_utilizada.add(dadosMaquina1.getRAM_utilizada());
-                RAM_naoutilizada.add(dadosMaquina1.getRAM_naoutilizada());
-                RAM_total.add(dadosMaquina1.getRAM_total());
-                RAM_disponivel.add(dadosMaquina1.getRAM_disponivel());
-                tempoAtivo.add(dadosMaquina1.getTempoAtivo());
-                tempoInativo.add(dadosMaquina1.getTempoInativo());
-                tempoCPU.add(dadosMaquina1.getTempoCPU());
-                ArmazenamentoDisponivel.add(dadosMaquina1.getArmazenamentoDisponivel());
-                ArmazenamentoIndisponivel.add(dadosMaquina1.getArmazenamentoIndisponivel());
-                ArmazenamentoTotal.add(dadosMaquina1.getArmazenamentoTotal());
-                Bateria.add(dadosMaquina1.getBateria());
-                CPU_Porcentos.add((double)dadosMaquina1.getCPU_Porcentos());
-                RAM_utilizadaPorcento.add((double)dadosMaquina1.getRAM_utilizadaPorcento());
-                RAM_naoutilizadaPorcento.add((double)dadosMaquina1.getRAM_naoutilizadaPorcento());
-                ArmazenamentoIndisponivelPorcento.add((double)dadosMaquina1.getArmazenamentoIndisponivelPorcento());
-                dadosMaquina1.repetir(repeticoes);
-
-
-            }
         System.out.flush();
 
         System.out.println("\nID: " + ID);
@@ -100,70 +91,50 @@ public class Main {
         System.out.println("RAM_naoutilizadaPorcento: " + RAM_naoutilizadaPorcento);
         System.out.println("ArmazenamentoIndisponivelPorcento: " + ArmazenamentoIndisponivelPorcento);
 
-
-        System.out.println("\nOh Não!! os dados estão tudo bagunçado!! quer que eu arrume?");
+        System.out.println("\nOh Não!! os dados estão bagunçados!! quer que eu arrume?");
         System.out.println("1-Organizar por ID \n2-Organizar por maior número \n3- Organizar por menor número\n4-Não quero");
         Integer escolha = input.nextInt();
-        // Thread.sleep(1000);
 
-        if (escolha == 1) {
-            for (int i = 0; i < ID.size(); i++) {
-                List<Double> ComponenteAtual = Componentes.get(i);
-                for (int j = i + 1; j < ComponenteAtual.size(); j++) {
-                    if (ComponenteAtual.get(i) < ComponenteAtual.get(j)) {
-                        // Swap elements
-                        double aux = ComponenteAtual.get(i);
-                        ComponenteAtual.set(i, ComponenteAtual.get(j));
-                        ComponenteAtual.set(j, aux);
-                    }
+        for (int i = 0; i < ID.size() - 1; i++) {
+            int indMenor = i;
+            for (int j = i + 1; j < ID.size(); j++) {
+                if (Integer.parseInt(ID.get(indMenor)) > Integer.parseInt(ID.get(j))) {
+                    indMenor = j;
                 }
             }
-            System.out.flush();
-            System.out.println("\nID: " + ID);
-            System.out.println("CPU_frequencia: " + CPU_frequencia);
-            System.out.println("RAM_utilizada: " + RAM_utilizada);
-            System.out.println("RAM_naoutilizada: " + RAM_naoutilizada);
-            System.out.println("RAM_total: " + RAM_total);
-            System.out.println("RAM_disponivel: " + RAM_disponivel);
-            System.out.println("tempoAtivo: " + tempoAtivo);
-            System.out.println("tempoInativo: " + tempoInativo);
-            System.out.println("tempoCPU: " + tempoCPU);
-            System.out.println("ArmazenamentoDisponivel: " + ArmazenamentoDisponivel);
-            System.out.println("ArmazenamentoIndisponivel: " + ArmazenamentoIndisponivel);
-            System.out.println("ArmazenamentoTotal: " + ArmazenamentoTotal);
-            System.out.println("Bateria: " + Bateria);
-            System.out.println("CPU_Porcentos: " + CPU_Porcentos);
-            System.out.println("RAM_utilizadaPorcento: " + RAM_utilizadaPorcento);
-            System.out.println("RAM_naoutilizadaPorcento: " + RAM_naoutilizadaPorcento);
-            System.out.println("ArmazenamentoIndisponivelPorcento: " + ArmazenamentoIndisponivelPorcento);
-        } else if (escolha.equals(2)) {
 
-        } else if (escolha.equals(3)) {
+            if (indMenor != i) {
+                String auxID = ID.get(i);
+                ID.set(i, ID.get(indMenor));
+                ID.set(indMenor, auxID);
 
-        }else {
-
+                for (int k = 1; k < Componentes.size(); k++) {
+                    List<String> componente = Componentes.get(k);
+                    String auxComponente = componente.get(i);
+                    componente.set(i, componente.get(indMenor));
+                    componente.set(indMenor, auxComponente);
+                }
+            }
         }
 
+        System.out.println("\nComponentes ordenados pelo ID:");
 
-
-
-//        System.out.println("CPU_porcento(%) : " + dadosMaquina1.getCPU_Porcentos() + "%");
-//        System.out.println("CPU_frequencia(GHz) : " + dadosMaquina1.getCPU_frequencia() + " GHz");
-//        System.out.println("RAM_utilizada(Gb) : " + dadosMaquina1.getRAM_utilizada()+ " Mb");
-//        System.out.println("RAM_naoutilizada(Gb) : " + dadosMaquina1.getRAM_naoutilizada()+ " Mb");
-//        System.out.println("RAM_total(Gb) : " + dadosMaquina1.getRAM_total()+ " Mb");
-//        System.out.println("RAM_disponivel(Gb): " + dadosMaquina1.getRAM_disponivel()+ " Mb");
-//        System.out.println("tempoAtivo(M) : " + dadosMaquina1.getTempoAtivo() + " Minutos");
-//        System.out.println("tempoInativo(M) : " + dadosMaquina1.getTempoInativo() + " Minutos");
-//        System.out.println("tempoCPU(M) : " + dadosMaquina1.getTempoCPU()+ " Minutos");
-//        System.out.println("ArmazenamentoDisponivel(Gb) : " + dadosMaquina1.getArmazenamentoDisponivel() + " Mb");
-//        System.out.println("ArmazenamentoIndisponivel(Gb) : " + dadosMaquina1.getArmazenamentoIndisponivel()+ " Mb");
-//        System.out.println("ArmazenamentoTotal(Gb) : " + dadosMaquina1.getArmazenamentoTotal()+ " Mb");
-//        System.out.println("Bateria: " + dadosMaquina1.getBateria() + "%");
-
-
-        }
-
+        System.out.println("ID: " + ID);
+        System.out.println("CPU_frequencia: " + CPU_frequencia);
+        System.out.println("RAM_utilizada: " + RAM_utilizada);
+        System.out.println("RAM_naoutilizada: " + RAM_naoutilizada);
+        System.out.println("RAM_total: " + RAM_total);
+        System.out.println("RAM_disponivel: " + RAM_disponivel);
+        System.out.println("tempoAtivo: " + tempoAtivo);
+        System.out.println("tempoInativo: " + tempoInativo);
+        System.out.println("tempoCPU: " + tempoCPU);
+        System.out.println("ArmazenamentoDisponivel: " + ArmazenamentoDisponivel);
+        System.out.println("ArmazenamentoIndisponivel: " + ArmazenamentoIndisponivel);
+        System.out.println("ArmazenamentoTotal: " + ArmazenamentoTotal);
+        System.out.println("Bateria: " + Bateria);
+        System.out.println("CPU_Porcentos: " + CPU_Porcentos);
+        System.out.println("RAM_utilizadaPorcento: " + RAM_utilizadaPorcento);
+        System.out.println("RAM_naoutilizadaPorcento: " + RAM_naoutilizadaPorcento);
+        System.out.println("ArmazenamentoIndisponivelPorcento: " + ArmazenamentoIndisponivelPorcento);
     }
-
-
+}
